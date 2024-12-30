7e8af8fd76fa446d3f4ecc731f250d982f0966e6
farrisric <farrisric@outlook.com>
2024-12-30 11:56:22 +0100

    license

--- mcpy/utils/utils.py
+++ mcpy/utils/utils.py
@@ -1,5 +1,6 @@
 from ase import Atoms
-from ase.geometry import get_layers
+from ase.neighborlist import NeighborList, natural_cutoffs
+
 
 def find_surface_indices(atoms: Atoms, tolerance: float = 0.1) -> list:
     """
@@ -12,35 +13,16 @@ def find_surface_indices(atoms: Atoms, tolerance: float = 0.1) -> list:
     Returns:
     list: A list of surface atom indices.
     """
-    # Get the layers of atoms
-    layers, _ = get_layers(atoms, tolerance=tolerance)
+    # Create a neighbor list
+    cutoffs = natural_cutoffs(atoms)
+    neighbor_list = NeighborList(cutoffs, self_interaction=False, bothways=True)
+    neighbor_list.update(atoms)
     
-    # Find the maximum layer index which corresponds to the surface
-    max_layer = max(layers)
-    
-    # Get the indices of atoms in the surface layer
-    surface_indices = [index for index, layer in enumerate(layers) if layer == max_layer]
+    surface_indices = []
+    for i in range(len(atoms)):
+        indices, offsets = neighbor_list.get_neighbors(i)
+        if len(indices) < 12:
+            print(len(indices))
+            surface_indices.append(i)
     
     return surface_indices
-
-    def find_nanoparticle_surface_indices(atoms: Atoms, tolerance: float = 0.1) -> list:
-        """
-        Find the surface indices of a nanoparticle atoms object.
-
-        Parameters:
-        atoms (Atoms): The atoms object representing the nanoparticle.
-        tolerance (float): The tolerance for identifying surface atoms.
-
-        Returns:
-        list: A list of surface atom indices.
-        """
-        # Get the layers of atoms
-        layers, _ = get_layers(atoms, tolerance=tolerance)
-        
-        # Find the maximum layer index which corresponds to the surface
-        max_layer = max(layers)
-        
-        # Get the indices of atoms in the surface layer
-        surface_indices = [index for index, layer in enumerate(layers) if layer == max_layer]
-        
-        return surface_indices
