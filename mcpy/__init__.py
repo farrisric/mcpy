12a67511c55d977ce70de4de88cc9ed070c86811
farrisric <farrisric@outlook.com>
2025-03-17 13:39:01 +0100

    pyproject updates

--- mcpy/__init__.py
+++
@@ -1,9 +0,0 @@
-from .ensembles.base_ensemble import BaseEnsemble
-from .ensembles.canonical_ensemble import CanonicalEnsemble
-from .ensembles.grand_canonical_ensemble import GrandCanonicalEnsemble
-
-__all__ = [
-    "BaseEnsemble",
-    "CanonicalEnsemble",
-    "GrandCanonicalEnsemble"
-]
