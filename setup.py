6e5ddc33c1f4187cf5ed46ec87aa83c8682217e4
farrisric <farrisric@outlook.com>
2025-03-17 13:39:01 +0100

    pyproject updates

--- setup.py
+++ setup.py
@@ -5,10 +5,5 @@ setup(
     version="0.1.0",
     description="A python package to run atomistic Monte Carlo simulations",
     packages=find_packages(),
-    install_requires=[
-        "ase>=3.23.0",
-        "mace-torch>=0.3.9",
-#        "mpi4py>=4.0.1",
-    ],
     python_requires='>=3.11',
 )
