29de09c3074e705e53a0f1a15e6b0d466ca698de
farrisric <farrisric@outlook.com>
2025-03-21 15:41:58 +0100

    overlap of spheres

--- mcpy/utils/utils.py
+++ mcpy/utils/utils.py
@@ -1,6 +1,7 @@
 from ase import Atoms
 from ase.neighborlist import NeighborList, natural_cutoffs
 from ase.cell import Cell
+import math
 
 
 def get_volume(box: list) -> float:
@@ -35,3 +36,38 @@ def find_surface_indices(atoms: Atoms, tolerance: float = 0.1) -> list:
             surface_indices.append(i)
 
     return surface_indices
+
+
+def sphere_volume(radius):
+    return (4/3) * math.pi * radius**3
+
+
+def total_volume(spheres):
+    total_vol = sum(sphere_volume(radius) for radius in spheres)
+    return total_vol
+
+
+def overlap_volume(r1, r2, d):
+    """Calculate the volume of overlap between two spheres of radius r1, r2 and distance d."""
+    if d >= r1 + r2:
+        return 0  # No overlap
+    if d <= abs(r1 - r2):
+        return (4/3) * math.pi * min(r1, r2)**3
+
+    part1 = (math.pi * (r1 + r2 - d)**2 * (d**2 + 2*d*(r1 + r2) - 3*(r1 - r2)**2)) / (12 * d)
+    part2 = (math.pi * (r1 + r2 - d)**2 * (d**2 + 2*d*(r1 + r2) - 3*(r1 - r2)**2)) / (12 * d)
+    return part1 + part2
+
+
+def total_volume_with_overlap(spheres, positions):
+    total_vol = 0
+    num_spheres = len(spheres)
+    for i in range(num_spheres):
+        total_vol += sphere_volume(spheres[i])
+    print('Total volue: ', total_vol)
+
+    for i in range(num_spheres):
+        for j in range(i + 1, num_spheres):
+            d = math.dist(positions[i], positions[j])  # Distance between sphere centers
+            total_vol -= overlap_volume(spheres[i], spheres[j], d)
+    return total_vol
