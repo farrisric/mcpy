fix units volume

examples/gcmc_relaxation.py
@@ -1,32 +1,39 @@
-from ase.build import fcc111
+from ase.build import fcc111, bulk, molecule
 from ase.constraints import FixAtoms
 import numpy as np
 
 from mcpy.moves import DeletionMove
-from mcpy.moves import BiasInsertionMove
+from mcpy.moves import InsertionMove
 from mcpy.moves.move_selector import MoveSelector
 from mcpy.ensembles.grand_canonical_ensemble import GrandCanonicalEnsemble
 from mcpy.calculators import MACE_F_Calculator
-from mcpy.utils.utils import get_volume
+from mcpy.utils.utils import get_volume, total_volume_with_overlap
 
-atoms = fcc111('Ag', size=(4, 4, 3), periodic=True, vacuum=8)
+R_RELAX = 2.068 
+
+atoms = fcc111('Ag', a=4.165, size=(4, 4, 3), periodic=True, vacuum=8)
 bottom_layer = [a.index for a in atoms if a.tag == 3]
 constraint = FixAtoms(indices=bottom_layer)
 atoms.set_constraint(constraint)
 
 surface_indices = [a.index for a in atoms if a.tag == 1]
-box = [atoms.cell[0], atoms.cell[1], np.array([0, 0, 6])]
-z_shift = atoms[surface_indices[0]].position[2]-3
+box = [atoms.cell[0], atoms.cell[1], np.array([0, 0, 7])]
+z_shift = atoms[surface_indices[0]].position[2]-R_RELAX
 
-atoms_in_box = [a for a in atoms if a.position[2] < z_shift+3 and a.position[2] > z_shift-3]
+atoms_in_box = [a for a in atoms if a.position[2] > z_shift]
 
-r_min = 1.3
 volume = get_volume(box)
-volume -= 4/3 * np.pi * (r_min**3) * len(atoms_in_box)
+print(volume)
+volume_with_overlap = total_volume_with_overlap(
+    [R_RELAX for _ in range(len(atoms_in_box))],
+    [a.position for a in atoms_in_box],)
+
+volume = volume - volume_with_overlap
+print(volume_with_overlap)
 print(volume)
 
 calculator = MACE_F_Calculator(
-                model_paths='/home/riccardo/Downloads/mace-large-density-agnesi-stress.model',
+                model_paths='/home/riccardo/Downloads/mace-small-density-agnesi-stress.model',
                 steps=20,
                 fmax=0.1,
                 cueq=False,
@@ -35,33 +42,49 @@ calculator = MACE_F_Calculator(
 
 species = ['Ag', 'O']
 
-move_list = [[0, 1],
+move_list = [[5, 5],
              [DeletionMove(species=species,
                            seed=1234678764,
                            operating_box=box,
                            z_shift=z_shift),
-              BiasInsertionMove(species=species,
-                                seed=6758763657,
-                                operating_box=box,
-                                min_insert=r_min,
-                                species_bias=['Ag'],
-                                z_shift=z_shift)]]
+              InsertionMove(species=species,
+                            seed=6758763657,
+                            operating_box=box,
+                            min_max_insert=[0.5, 3],
+                            z_shift=z_shift)]]
 
 move_selector = MoveSelector(*move_list)
+
+o2 = molecule('O2')
+ag = bulk('Ag', a=4.165)
+calculator.steps = 100
+calculator.fmax = 0.05
+e_o2 = calculator.get_potential_energy(o2)
+e_ag = calculator.get_potential_energy(ag)
+    
+mus = {'Ag': -1, 'O': e_o2/2}
+delta_mu_O = -0.3
+mus['O'] += delta_mu_O
 T = 500
+
+calculator.steps = 20
+calculator.fmax = 0.1
+
 gcmc = GrandCanonicalEnsemble(
             atoms=atoms,
             calculator=calculator,
-            mu={'Ag' : -2.8, 'O' : -5.0},
+            mu=mus,
             units_type='metal',
             species=species,
-            species_bias={'Ag': 1.3},
+            species_bias={'Ag': R_RELAX},
             volume=volume,
             temperature=T,
             move_selector=move_selector,
-            outfile=f'gcmc_relax_{atoms.get_chemical_formula()}.out',
+            outfile=f'gcmc_relax_{atoms.get_chemical_formula()}_dmu_{delta_mu_O}.out',
             trajectory_write_interval=1,
             outfile_write_interval=1,
-            traj_file=f'gcmc_relax_{atoms.get_chemical_formula()}.xyz')
+            traj_file=f'gcmc_relax_{atoms.get_chemical_formula()}_dmu_{delta_mu_O}.xyz',
+            box=box,
+            z_shift=z_shift)
 
 gcmc.run(1000000)