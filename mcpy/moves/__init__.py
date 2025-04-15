b2fcbe3e9b3a3176c6742df206aa8b4a3778e07c
farrisric <farrisric@outlook.com>
2025-04-15 11:45:01 +0200

    fixes

--- mcpy/moves/__init__.py
+++ mcpy/moves/__init__.py
@@ -4,3 +4,12 @@ from .brownian_move import BrownianMove
 from .insertion_move import InsertionMove
 from .deletion_move import DeletionMove
 from .displacement_move import DisplacementMove
+
+__all__ = [
+    "MoveSelector",
+    "ShakeMove",
+    "BrownianMove",
+    "DeletionMove",
+    "DisplacementMove",
+    "InsertionMove"
+]
