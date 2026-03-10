ad0f6163a7162b4372bfe0da96038a4fe225f210
Riccardo Farris <farrisric@outlook.com>
2026-03-10 15:16:33 +0100

    created notebook section with example of gcmc for ag nanoparticle

--- mcpy/calculators/__init__.py
+++ mcpy/calculators/__init__.py
@@ -1,5 +1,7 @@
 from .mace_calculator import MACECalculator
 from .mace_f_calculator import MACE_F_Calculator
+from .base_calculator import BaseCalculator
 
 __all__ = ['MACECalculator',
-           'MACE_F_Calculator']
+           'MACE_F_Calculator',
+           'BaseCalculator']
